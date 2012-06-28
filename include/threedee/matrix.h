20db53c59bea992c45e1f6dd19b7d0b0013f2e94
Riku Salminen <riku.salminen@tkk.fi>
2012-06-29 00:00:14 +0300

    Scalar matrix inverses for 4x4 and 3x3

--- include/threedee/matrix.h
+++ include/threedee/matrix.h
@@ -222,4 +222,84 @@ static inline mat4 minverse(mat4 m)
     return mtranspose(minverse_transpose_rows(m.rows[0], m.rows[1], m.rows[2], m.rows[3]));
 }
 
+static inline mat4 minverse_scalar(mat4 mat)
+{
+    mat4 result;
+    float *m = (float*)&mat, *inv = (float*)&result;
+
+    inv[0] =   m[5]*m[10]*m[15] - m[5]*m[11]*m[14] - m[9]*m[6]*m[15]
+             + m[9]*m[7]*m[14] + m[13]*m[6]*m[11] - m[13]*m[7]*m[10];
+    inv[4] =  -m[4]*m[10]*m[15] + m[4]*m[11]*m[14] + m[8]*m[6]*m[15]
+             - m[8]*m[7]*m[14] - m[12]*m[6]*m[11] + m[12]*m[7]*m[10];
+    inv[8] =   m[4]*m[9]*m[15] - m[4]*m[11]*m[13] - m[8]*m[5]*m[15]
+             + m[8]*m[7]*m[13] + m[12]*m[5]*m[11] - m[12]*m[7]*m[9];
+    inv[12] = -m[4]*m[9]*m[14] + m[4]*m[10]*m[13] + m[8]*m[5]*m[14]
+             - m[8]*m[6]*m[13] - m[12]*m[5]*m[10] + m[12]*m[6]*m[9];
+    inv[1] =  -m[1]*m[10]*m[15] + m[1]*m[11]*m[14] + m[9]*m[2]*m[15]
+             - m[9]*m[3]*m[14] - m[13]*m[2]*m[11] + m[13]*m[3]*m[10];
+    inv[5] =   m[0]*m[10]*m[15] - m[0]*m[11]*m[14] - m[8]*m[2]*m[15]
+             + m[8]*m[3]*m[14] + m[12]*m[2]*m[11] - m[12]*m[3]*m[10];
+    inv[9] =  -m[0]*m[9]*m[15] + m[0]*m[11]*m[13] + m[8]*m[1]*m[15]
+             - m[8]*m[3]*m[13] - m[12]*m[1]*m[11] + m[12]*m[3]*m[9];
+    inv[13] =  m[0]*m[9]*m[14] - m[0]*m[10]*m[13] - m[8]*m[1]*m[14]
+             + m[8]*m[2]*m[13] + m[12]*m[1]*m[10] - m[12]*m[2]*m[9];
+    inv[2] =   m[1]*m[6]*m[15] - m[1]*m[7]*m[14] - m[5]*m[2]*m[15]
+             + m[5]*m[3]*m[14] + m[13]*m[2]*m[7] - m[13]*m[3]*m[6];
+    inv[6] =  -m[0]*m[6]*m[15] + m[0]*m[7]*m[14] + m[4]*m[2]*m[15]
+             - m[4]*m[3]*m[14] - m[12]*m[2]*m[7] + m[12]*m[3]*m[6];
+    inv[10] =  m[0]*m[5]*m[15] - m[0]*m[7]*m[13] - m[4]*m[1]*m[15]
+             + m[4]*m[3]*m[13] + m[12]*m[1]*m[7] - m[12]*m[3]*m[5];
+    inv[14] = -m[0]*m[5]*m[14] + m[0]*m[6]*m[13] + m[4]*m[1]*m[14]
+             - m[4]*m[2]*m[13] - m[12]*m[1]*m[6] + m[12]*m[2]*m[5];
+    inv[3] =  -m[1]*m[6]*m[11] + m[1]*m[7]*m[10] + m[5]*m[2]*m[11]
+             - m[5]*m[3]*m[10] - m[9]*m[2]*m[7] + m[9]*m[3]*m[6];
+    inv[7] =   m[0]*m[6]*m[11] - m[0]*m[7]*m[10] - m[4]*m[2]*m[11]
+             + m[4]*m[3]*m[10] + m[8]*m[2]*m[7] - m[8]*m[3]*m[6];
+    inv[11] = -m[0]*m[5]*m[11] + m[0]*m[7]*m[9] + m[4]*m[1]*m[11]
+             - m[4]*m[3]*m[9] - m[8]*m[1]*m[7] + m[8]*m[3]*m[5];
+    inv[15] =  m[0]*m[5]*m[10] - m[0]*m[6]*m[9] - m[4]*m[1]*m[10]
+             + m[4]*m[2]*m[9] + m[8]*m[1]*m[6] - m[8]*m[2]*m[5];
+
+    float det = m[0]*inv[0] + m[1]*inv[4] + m[2]*inv[8] + m[3]*inv[12];
+
+    for(int i = 0; i < 16; i++)
+        inv[i] = inv[i] * (1.0 / det);
+
+    return result;
+}
+
+static inline mat4 minverse3_scalar(mat4 mat)
+{
+    mat4 result;
+    float *m = (float*)&mat, *inv = (float*)&result;
+
+    float inv_det = 1.0 / (
+            m[0] * (m[5]*m[10] - m[6]*m[9])
+            - m[4] * (m[1]*m[10] - m[2]*m[9])
+            + m[8] * (m[1]*m[6] - m[2]*m[5]));
+
+    inv[0] = inv_det * (m[5]*m[10] - m[9]*m[6]);
+    inv[1] = inv_det * -(m[1]*m[10] - m[9]*m[2]);
+    inv[2] = inv_det * (m[1]*m[6] - m[2]*m[5]);
+
+    inv[4] = inv_det * -(m[4]*m[10] - m[6]*m[8]);
+    inv[5] = inv_det * (m[0]*m[10] - m[2]*m[8]);
+    inv[6] = inv_det * -(m[4]*m[6] - m[2]*m[4]);
+
+    inv[8] = inv_det * (m[4]*m[9] - m[5]*m[8]);
+    inv[9] = inv_det * -(m[0]*m[9] - m[1]*m[8]);
+    inv[10] = inv_det * (m[0]*m[5] - m[4]*m[1]);
+
+    inv[3] = inv[7] = inv[11] = 0.0; // 4th row
+    inv[12] = inv[13] = inv[14] = 0.0; // 4th column
+    inv[15] = 1.0;
+
+    return result;
+}
+
+static inline mat4 minverse3(mat4 mat)
+{
+    return minverse(mat3_to_mat4(mat));
+}
+
 #endif
