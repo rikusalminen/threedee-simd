c07e52b90816cabd1d6480db2c70590b7fe757c2
Aktau <nicolashillegeer@gmail.com>
2013-11-03 14:12:43 +0100

    better testing of quat_from_axisangle

--- test/test.c
+++ test/test.c
@@ -139,6 +139,43 @@ static void test_quat_to_mat() {
             compare_mat_equal(rot_y_45_ref, mat_from_qua, name);
         }
     }
+
+    const float cos30 = cosf(T_PI6);
+    const float sin30 = sinf(T_PI6);
+
+
+    {
+        mat4 rot_x_30_ref = {{
+            { 1,        0,      0,      0 },
+            { 0,        cos30,  sin30,  0 },
+            { 0,        -sin30, cos30,  0 },
+            { 0,        0,      0,      1 }
+        }};
+
+        vec4 axisangle = vec(1.0f, 0.0f, 0.0f, T_PI6);
+        vec4 qua       = quat_axisangle(axisangle);
+
+        {
+            const char *name  = "quaternion to matrix, rotate 30 deg around x (default)";
+            mat4 mat_from_qua = quat_to_mat(qua);
+
+            compare_mat_equal(rot_x_30_ref, mat_from_qua, name);
+        }
+
+        {
+            const char *name  = "quaternion to matrix, rotate 30 deg around x (mmul)";
+            mat4 mat_from_qua = quat_to_mat_mmul(qua);
+
+            compare_mat_equal(rot_x_30_ref, mat_from_qua, name);
+        }
+
+        {
+            const char *name  = "quaternion to matrix, rotate 30 deg around x (scalar)";
+            mat4 mat_from_qua = quat_to_mat_scalar(qua);
+
+            compare_mat_equal(rot_x_30_ref, mat_from_qua, name);
+        }
+    }
 }
 
 int main() {
